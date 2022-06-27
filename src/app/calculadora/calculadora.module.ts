ff7a8727a8b67c66a3f88af7839ddfb108f34c56
andre10dias <andre10dias@ymail.com>
2022-06-27 20:13:49 -0300

    Componente calculadora

--- src/app/calculadora/calculadora.module.ts
+++ src/app/calculadora/calculadora.module.ts
@@ -2,14 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CalculadoraComponent } from './calculadora.component';
 
-
-
 @NgModule({
   declarations: [
     CalculadoraComponent
   ],
   imports: [
     CommonModule
+  ],
+  exports: [
+    CalculadoraComponent
   ]
 })
 export class CalculadoraModule { }
